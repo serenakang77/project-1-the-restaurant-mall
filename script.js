6484a94ceee3ffe98d70698af421d8ef6434b69c
serenakang <serenakang77@gmail.com>
2022-09-27 20:45:35 -0400

    changed some javascript code

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
-$(document).ready(() => {
+// $(document).ready(() => {
 
-    const element = document.querySelector(".menuBar");
-    const menuBarElement = document.querySelector(".menuBarButton");
+//     const element = document.querySelector(".menuBar");
+//     const menuBarElement = document.querySelector(".menuBarButton");
 
-    $(".menuBarButton").on("click", function(event){
-        event.preventDefault();
-        element.style.visibility = "visible";
-        element.style.opacity = "1";
-        menuBarElement.style.visibility = "hidden";
-    })
+//     $(".menuBarButton").on("click", function(event){
+//         event.preventDefault();
+//         element.style.visibility = "visible";
+//         element.style.opacity = "1";
+//         menuBarElement.style.visibility = "hidden";
+//     })
 
-    $(".closeButton").on("click", function(event){
-        event.preventDefault();
-        menuBarElement.style.visibility = "visible";
-        element.style.visibility = "hidden";
-        element.style.opacity = "0";
-        element.style.transition = "visibility 0.5s, opacity 0.5s linear"
-    })
+//     $(".closeButton").on("click", function(event){
+//         event.preventDefault();
+//         menuBarElement.style.visibility = "visible";
+//         element.style.visibility = "hidden";
+//         element.style.opacity = "0";
+//         element.style.transition = "visibility 0.5s, opacity 0.5s linear"
+//     })
+// })
+
+const element = document.querySelector(".menuBar");
+const menuBarElement = document.querySelector(".menuBarButton");
+const closeElement = document.querySelector(".closeButton");
+
+menuBarElement.addEventListener("click", function(){
+    element.style.visibility = "visible";
+    element.style.opacity = "1";
+    menuBarElement.style.visibility = "hidden";
+})
+
+closeElement.addEventListener("click", function(){
+    menuBarElement.style.visibility = "visible";
+    element.style.visibility = "hidden";
+    element.style.opacity = "0";
+    element.style.transition = "visibility 0.5s, opacity 0.5s linear";
 })
